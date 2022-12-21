a7ee7120295c70a4a56240f65eca6d96fa978443
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2022-12-22 00:02:21 +0200

    Update submodules and enable LTO

--- learn_opengl/src/opengl/debug/annotations.cpp
+++ learn_opengl/src/opengl/debug/annotations.cpp
@@ -2,10 +2,17 @@
 
 #include "opengl/gl_api.hpp"
 
-ScopeAnnotation::ScopeAnnotation(std::string_view scope_name,
-                                 size_t id) noexcept {
+ScopeAnnotation::ScopeAnnotation([[maybe_unused]] std::string_view scope_name,
+                                 [[maybe_unused]] size_t id) noexcept {
+#ifdef ENABLE_RENDERING_ANNOTATIONS
   glPushDebugGroupKHR(GL_DEBUG_SOURCE_APPLICATION, static_cast<GLuint>(id),
                       static_cast<GLsizei>(scope_name.size()),
                       scope_name.data());
+#endif
 }
-ScopeAnnotation::~ScopeAnnotation() noexcept { glPopDebugGroupKHR(); }
+
+ScopeAnnotation::~ScopeAnnotation() noexcept {
+#ifdef ENABLE_RENDERING_ANNOTATIONS
+    glPopDebugGroupKHR();
+#endif
+}
